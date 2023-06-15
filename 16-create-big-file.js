220a5e15084feff895733668c8cda6e49fe8eb94
xz-1 <skonchan199w@gmail.com>
2023-06-15 18:00:58 -0400

    Chunk streaming commit

--- 16-create-big-file.js
+++ 16-create-big-file.js
@@ -1,5 +1,5 @@
 const { writeFileSync } = require('fs')
-for (let i = 0; i < 1000; i++) {
+for (let i = 0; i < 10000; i++) {
     writeFileSync('./content/big.txt', `Hello World ${i}\n`, { flag: 'a' })
 }
 
